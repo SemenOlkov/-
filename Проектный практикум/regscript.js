9752ffbfeaa56e9d589d6372ac502b3e04ab467f
Semen <165212317+SemenOlkov@users.noreply.github.com>
2024-05-04 17:43:49 +0500

    Add files via upload

--- Проектный практикум/regscript.js
+++ Проектный практикум/regscript.js
@@ -38,3 +38,11 @@ document.getElementById('5to6').addEventListener('click', function() {
     document.getElementById('5').classList.add('hidden');
     document.getElementById('6').classList.remove('hidden');
 });
+document.getElementById('6to7').addEventListener('click', function() {
+    document.getElementById('6').classList.add('hidden');
+    document.getElementById('7').classList.remove('hidden');
+});
+document.getElementById('7to6').addEventListener('click', function() {
+    document.getElementById('7').classList.add('hidden');
+    document.getElementById('6').classList.remove('hidden');
+});
