afe67b9bb82262294e95402a10a232212831ea02
Semen <165212317+SemenOlkov@users.noreply.github.com>
2024-05-16 00:21:33 +0500

    Add files via upload

--- underindex/fixforeng.js
+++ underindex/fixforeng.js
@@ -14,3 +14,27 @@ document.getElementById("testubutton").addEventListener("click", function() {
         hiddenBlock.classList.add("hidden");
     }
 });
+document.getElementById("demonstraterules").addEventListener("click", function() {
+    var hiddenBlock = document.getElementById("rules");
+    if (hiddenBlock.classList.contains("hidden")) {
+        hiddenBlock.classList.remove("hidden");
+    } else {
+        hiddenBlock.classList.add("hidden");
+    }
+});
+document.getElementById("proctorrules").addEventListener("click", function() {
+    var hiddenBlock = document.getElementById("rules1");
+    if (hiddenBlock.classList.contains("hidden")) {
+        hiddenBlock.classList.remove("hidden");
+    } else {
+        hiddenBlock.classList.add("hidden");
+    }
+});
+document.getElementById("rights").addEventListener("click", function() {
+    var hiddenBlock = document.getElementById("rules2");
+    if (hiddenBlock.classList.contains("hidden")) {
+        hiddenBlock.classList.remove("hidden");
+    } else {
+        hiddenBlock.classList.add("hidden");
+    }
+});
